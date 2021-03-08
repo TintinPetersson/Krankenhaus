d5d5a7f3d5aaab27a33a822460ee26031d26721a
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:40:23 +0100

    Create Sanatorium.cs

---
+++ Krankenhaus/Backend/Sanatorium.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    class Sanatorium
+    {
+        private List<Patient> patients;
+
+        public int NumberOfBeds { get; set; }
+        public int OccupiedBeds { get; set; }
+        public bool IsFull { get => NumberOfBeds - OccupiedBeds <= 0; }
+
+        public Sanatorium()
+        {
+            patients = new List<Patient>();
+        }
+
+        public bool CheckIn(Patient patient)
+        {
+            if (IsFull)
+            {
+                return false;
+            }
+
+            patients.Add(patient);
+            return true;
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
