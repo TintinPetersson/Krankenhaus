f58f2dc8802b4d88c1bf647b3bcbeb6b56944dff
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:40:40 +0100

    Create Logger.cs

---
+++ Krankenhaus/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    class Logger
+    {
+        public void LogToFileHandler(object sender, CustomEventArgs c)
+        {
+            using (StreamWriter writer = new StreamWriter("CurrentTime.txt", true))
+            {
+                writer.WriteLine(c.timeNow);
+                writer.Flush();
+            }
+        }
+    }
+}
