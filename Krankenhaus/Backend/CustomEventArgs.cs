cf6e3a7b989ab77dcaf6ac0fadfe576b427c1421
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:39:54 +0100

    Create CustomEventArgs.cs

---
+++ Krankenhaus/Backend/CustomEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    public class CustomEventArgs : EventArgs
+    {
+        public string timeNow { get; set; }
+        
+    }
+}
