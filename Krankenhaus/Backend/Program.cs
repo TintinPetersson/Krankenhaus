352326432f7e64abff8e749d5a1550478892faa2
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:40:19 +0100

    Create Program.cs

---
+++ Krankenhaus/Backend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generator generator = new Generator();
+            generator.Start();
+            Console.ReadLine();
+        }
+    }
+}
