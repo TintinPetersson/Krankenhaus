f79afd115d55c0ba79ac76d6b5864900c7c88e3f
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:40:32 +0100

    Create Ticker.cs

---
+++ Krankenhaus/Backend/Ticker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    class Ticker
+    {
+        public event EventHandler<CustomEventArgs> Tick;
+        private bool keepTicking;
+        public async Task Ticking(int time)
+        {
+            keepTicking = true;
+
+            while (keepTicking)
+            {
+                await Task.Delay(time * 1000);
+                Tick?.Invoke(this, new CustomEventArgs());
+            }
+        }
+
+        public void StopTick()
+        {
+            keepTicking = false;
+        }
+    }
+}
