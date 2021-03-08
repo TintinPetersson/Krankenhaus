ce3a1b15a73074e381221e7e917673643aee7d84
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-03-08 11:40:37 +0100

    Create Frontend.cs

---
+++ Krankenhaus/Frontend/Frontend.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankenhaus
+{
+    class Frontend
+    {
+        public int Menu()
+        {
+            do
+            {
+                Console.WriteLine("Choose the speed of the program [1-10 seconds]: ");
+                bool check = int.TryParse(Console.ReadLine(), out int result);
+                if (result <= 10 && result >= 1)
+                {
+                    return result;
+                }
+            } while (true);
+        }
+    }
+}
